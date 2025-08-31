c08ef41420c9ffaac049c8ca42df93f7f2e1e07e
Serhii-Filatov <55346150+Serhii-Filatov@users.noreply.github.com>
2025-09-01 01:04:58 +0300

    feat: added welcome page

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,24 @@ import BaseButton from '@/components/BaseButton/BaseButton.vue'
 </script>
 
 <template>
-  <h1>HELLO VUE</h1>
-  <BaseButton />
+  <main class="flex h-screen">
+    <section class="flex flex-1 justify-center items-center px-5 bg-primary">
+      <div class="text-white text-center">
+        <img class="inline mb-6" src="./assets/image/map-pin.svg" alt="map icon" />
+        <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
+        <p class="leading-6 mb-11">
+          Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
+          ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
+        </p>
+        <BaseButton></BaseButton>
+      </div>
+    </section>
+    <section class="flex-1">
+      <img
+        class="h-full w-full object-cover"
+        src="./assets/image/static-map.png"
+        alt="static map image"
+      />
+    </section>
+  </main>
 </template>
--- src/components/BaseButton/BaseButton.vue
+++ src/components/BaseButton/BaseButton.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="bg-[#ff2a79] rounded-xl py-3 px-10 text-white font-bold -tracking-wider"
+    class="bg-[#ffa279] rounded-xl py-3 px-10 text-white font-bold -tracking-wider"
     type="button"
   >
     Почати роботу
