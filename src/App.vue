440a5c4032980136ab8dce4647df6298d79b8e10
Serhii-Filatov <55346150+Serhii-Filatov@users.noreply.github.com>
2025-08-31 23:47:05 +0300

    feat(ui): add base button component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
+<script setup>
+import BaseButton from '@/components/BaseButton/BaseButton.vue'
+</script>
+
 <template>
   <h1>HELLO VUE</h1>
+  <BaseButton />
 </template>
-
---
+++ src/components/BaseButton/BaseButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="bg-[#ff2a79] rounded-xl py-3 px-10 text-white font-bold -tracking-wider"
+    type="button"
+  >
+    Почати роботу
+  </button>
+</template>
+
+<style scoped></style>
